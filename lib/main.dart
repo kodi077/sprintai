8fb0528286b722f1dc9faa109c83ca9844cab495
kodi-tourlive <kodi@tourlive.co.kr>
2026-02-27 09:56:31 +0900

    Init Supabase app boot

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'config/app_constants.dart';
 import 'config/app_theme.dart';
+import 'config/supabase_config.dart';
 import 'providers/app_state.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
+
   runApp(const SprintAiApp());
 }
 
